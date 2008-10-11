8c74b38bdf06f7e1abb4e98ae8809e9ad59d49a9
Rich Lane <lanerl@gmail.com>
2008-10-11 15:06:23 -0400

    dummy x86_64 assembler

---
+++ arch/x86_64.rb
@@ -0,0 +1,28 @@
+class Assembler
+	def initialize
+		clear
+	end
+
+	def assemble(src=nil, &b)
+		if src
+			instance_eval { eval src }
+		else
+			instance_eval(&b)
+		end
+		out = @out
+		clear
+		out
+	end
+
+	private
+
+	def clear
+		@out = String.new
+		@next_reg = 1
+	end
+
+	def literal(x)
+		@out << "#{x}\n"
+	end
+
+end
--- test/x86_64/asm/simple.rb
+++ test/x86_64/asm/simple.rb
@@ -1,3 +1,5 @@
+require 'arch/x86_64'
+
 class X86_64Test
 
 def test_ret
@@ -9,7 +11,12 @@ def test_ret
 			syscall
 	EOF
 
+	asm2 = Assembler.new.assemble {
+		literal asm
+	}
+
 	run_test asm, 42
+	run_test asm2, 42
 end
 
 end
