7a5782ab7910e02607fa6091ea538732ef6b565a
Rich Lane <lanerl@gmail.com>
2008-10-24 12:20:09 -0400

    fib testcase

--- test/x86_64/asm/functions.rb
+++ test/x86_64/asm/functions.rb
@@ -18,4 +18,64 @@ def test_call
 	run_test bin, 5
 end
 
+# http://en.wikibooks.org/wiki/Fibonacci_number_program#Arithmetic_version_2
+def fib(n)
+  ((((1+Math.sqrt(5))/2)**n)/Math.sqrt(5)+0.5).floor
+end
+
+def test_fib
+	testargs = [0,1,2,10]
+
+	testargs.each do |arg|
+		expected = fib(arg)
+
+		bin = assemble {
+			m = addmacros DefaultMacros
+			arg1 = rdi
+			rval = rax
+			tmp = rbx
+
+			m.entry[:main]
+			mov arg, arg1
+			call :fib
+			cmp expected, rval
+			jne :fail
+			m.sys_exit[0]
+			label :fail
+			m.sys_exit[1]
+
+			# function fib(x)
+			m.entry[:fib]
+			cmp 1, arg1
+			jg :greater_than_one
+			# x <= 1
+			m.return[arg1]
+			# x > 1
+			label :greater_than_one
+			push tmp # callee save
+			push arg1
+			# tmp <= fib(x-1)
+			sub 1, arg1
+			call :fib
+			mov rval, tmp
+			# rval <= fib(x-2)
+			pop arg1
+			sub 2, arg1
+			call :fib
+			# return rval + tmp
+			add tmp, rval
+			pop tmp # callee restore
+			m.return[rval]
+		}
+
+		begin
+			run_test bin, 0
+		rescue
+			puts "arg: #{arg}"
+			raise
+		end
+	end
+
+end
+
 end
