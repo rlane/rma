bba123d58fd280262bab77f6c3001e1adcb775a5
Rich Lane <lanerl@gmail.com>
2008-10-24 12:20:02 -0400

    jump instructions

--- arch/x86_64/instructions.rb
+++ arch/x86_64/instructions.rb
@@ -21,9 +21,19 @@ class RMA::X86_64::Assembler
 	op 'mov', any(Reg, Imm, Mem), any(Reg, Mem)
 	op 'movq', any(Reg, Imm, Mem), any(Reg, Mem)
 	op 'add', any(Reg, Imm, Mem), any(Reg, Mem)
+	op 'sub', any(Reg, Imm, Mem), any(Reg, Mem)
+	op 'cmp', any(Reg, Imm), Reg
 	op 'lea', Mem, Reg
 	op 'ret'
 	op 'syscall'
 	op 'jmp', Label
 	op 'call', Label
+	op 'push', any(Reg, Imm)
+	op 'pop', Reg
+
+	%w(jmp je jz jcxz jp jpe jne jnz jecxz jnp jpo 
+	   ja jae jb jbe jna jnae jnb jnbe jc jnc jg jge 
+		 jl jle jng jnge jnl jnle jo jno js jns).each do |jmpop|
+		op jmpop, Label
+	end
 end
