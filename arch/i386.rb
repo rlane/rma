455bba7eaaca92151fce500584327a12afaf5ca5
Rich Lane <lanerl@gmail.com>
2008-10-25 19:38:00 -0400

    added i386 hlt

--- arch/i386.rb
+++ arch/i386.rb
@@ -16,7 +16,7 @@ class RMA::I386::Assembler < RMA::X86::Assembler
 	make_special_regs %w(gs fs es ds)
 
 	no_arg = lambda { |name| op name }
-	%w(nop ret iret cpuid leave).each &no_arg
+	%w(nop ret iret cpuid leave hlt).each &no_arg
 	%w(cbw cwd cwde).each &no_arg
 	%w(pushf popf pusha popa).each &no_arg
 	%w(stc clc cmc std cld sti cli).each &no_arg
